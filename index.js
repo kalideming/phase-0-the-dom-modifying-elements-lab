d8f1bc72ff1670b7289aab48c602af549b30fd64
Kali Deming <kalideming@icloud.com>
2022-11-15 09:59:33 -0700

    commit tree f932bfbc904e49ccf33d95604fc6e156b257e316

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+const main = document.querySelector("#main");
+main.remove();
+const body = document.querySelector("body");
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "Kali is the champion";
